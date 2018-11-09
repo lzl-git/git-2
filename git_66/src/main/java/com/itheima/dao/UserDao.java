a46b4fdb8d269e45c40dc19ef039c8d18be3ee95
123 <123@li.cn>
2018-11-09 10:53:00 +0800

    sdsdsd

---
+++ git_66/src/main/java/com/itheima/dao/UserDao.java
@@ -0,0 +1,8 @@
+package com.itheima.dao;
+
+public class UserDao {
+    public static void main(String[] args) {
+        System.out.println("123456");
+        System.out.println("4565");
+    }
+}
---
+++ git_66/src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,13 @@
+package com.itheima.domain;
+
+public class User {
+    private String Username;
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+}
