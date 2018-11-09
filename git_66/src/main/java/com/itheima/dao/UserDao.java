18232a753b63bf2d3da9d21ba029f67e0b456d96
123 <123@li.cn>
2018-11-09 10:54:53 +0800

    dgdff

--- git_66/src/main/java/com/itheima/dao/UserDao.java
+++ git_66/src/main/java/com/itheima/dao/UserDao.java
@@ -4,5 +4,6 @@ public class UserDao {
     public static void main(String[] args) {
         System.out.println("123456");
         System.out.println("4565");
+        System.out.println("8494854");
     }
 }
